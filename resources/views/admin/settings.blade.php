bc4f9879a29e5e211e8209195fab811bb733c58e
felixparejapmdit <155054492+felixparejapmdit@users.noreply.github.com>
2024-09-17 10:34:23 +0800

    update

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -1,14 +1,3 @@
-<!-- music_management/categories.blade.php -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-<!-- Include Bootstrap CSS -->
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Include jQuery before Bootstrap -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-<!-- resources/views/admin/settings.blade.php -->
-
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -20,15 +9,15 @@
         <div class="container mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="d-flex flex-wrap">
+                    <div class="d-flex flex-wrap justify-content-center">
 
-                           <!-- List of Import Docx -->
-                           <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <!-- List of Import Docx -->
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
                                     <h5>
                                         <a href="{{ route('verse_search') }}" class="settings_button">
-                                        <i class="fas fa-file-word fa-4x icon" aria-hidden="true"></i>
+                                            <i class="fas fa-file-import fa-4x icon" aria-hidden="true"></i>
                                             <br><br>
                                             <span class="name">Import Word Docx</span>
                                             <span class="keywords" aria-hidden="true" style="display:none">Import_Docx, management</span>
@@ -45,7 +34,7 @@
                                 <div class="box-body text-center mt-4">
                                     <h5>
                                         <a href="{{ route('events.index') }}" class="settings_button">
-                                        <i class="fas fa-calendar fa-4x icon" aria-hidden="true"></i>
+                                            <i class="fas fa-calendar-alt fa-4x icon" aria-hidden="true"></i>
                                             <br><br>
                                             <span class="name">Events</span>
                                             <span class="keywords" aria-hidden="true" style="display:none">events, management</span>
@@ -56,13 +45,13 @@
                             </div>
                         </div>
 
-                               <!-- List of event types -->
-                               <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <!-- List of event types -->
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
                                     <h5>
                                         <a href="{{ route('eventtypes.index') }}" class="settings_button">
-                                        <i class="fas fa-calendar fa-4x icon" aria-hidden="true"></i>
+                                            <i class="fas fa-list-alt fa-4x icon" aria-hidden="true"></i>
                                             <br><br>
                                             <span class="name">Event Types</span>
                                             <span class="keywords" aria-hidden="true" style="display:none">event types, management</span>
@@ -73,14 +62,13 @@
                             </div>
                         </div>
 
-
-                                     <!-- List of districts -->
-                                     <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <!-- List of districts -->
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
                                     <h5>
                                         <a href="{{ route('districts.index') }}" class="settings_button">
-                                        <i class="fas fa-calendar fa-4x icon" aria-hidden="true"></i>
+                                            <i class="fas fa-map-marker-alt fa-4x icon" aria-hidden="true"></i>
                                             <br><br>
                                             <span class="name">Districts</span>
                                             <span class="keywords" aria-hidden="true" style="display:none">districts, management</span>
@@ -91,14 +79,13 @@
                             </div>
                         </div>
 
-
-                                     <!-- List of locale_congregations -->
-                                     <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <!-- List of locale_congregations -->
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
                                     <h5>
                                         <a href="{{ route('locale_congregations.index') }}" class="settings_button">
-                                        <i class="fas fa-calendar fa-4x icon" aria-hidden="true"></i>
+                                            <i class="fas fa-building fa-4x icon" aria-hidden="true"></i>
                                             <br><br>
                                             <span class="name">Locale Congregations</span>
                                             <span class="keywords" aria-hidden="true" style="display:none">locale_congregations, management</span>
@@ -109,15 +96,13 @@
                             </div>
                         </div>
 
-                        
-
-                                     <!-- List of lessons -->
-                                     <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <!-- List of lessons -->
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
                                     <h5>
                                         <a href="{{ route('lessons.index') }}" class="settings_button">
-                                        <i class="fas fa-book fa-4x icon" aria-hidden="true"></i>
+                                            <i class="fas fa-book-reader fa-4x icon" aria-hidden="true"></i>
                                             <br><br>
                                             <span class="name">Lessons</span>
                                             <span class="keywords" aria-hidden="true" style="display:none">lessons, management</span>
@@ -128,33 +113,30 @@
                             </div>
                         </div>
 
-
-
                         <!-- List of translations -->
                         <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
                                     <h5>
                                         <a href="{{ route('translations.index') }}" class="settings_button">
-                                        <i class="fas fa-language fa-4x icon" aria-hidden="true"></i>
+                                            <i class="fas fa-language fa-4x icon" aria-hidden="true"></i>
                                             <br><br>
                                             <span class="name">Translations</span>
                                             <span class="keywords" aria-hidden="true" style="display:none">translations, management</span>
                                         </a>
-                                    </h5>
+ </h5>
                                     <p class="help-block" style="display:none">Manage translations</p>
                                 </div>
                             </div>
                         </div>
 
-
-                                 <!-- List of PV Information -->
-                                 <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <!-- List of PV Information -->
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
                                     <h5>
                                         <a href="{{ route('pvinfo.index') }}" class="settings_button">
-                                        <i class="fas fa-info-circle fa-4x icon" aria-hidden="true"></i>
+                                            <i class="fas fa-info-circle fa-4x icon" aria-hidden="true"></i>
                                             <br><br>
                                             <span class="name">PV Information</span>
                                             <span class="keywords" aria-hidden="true" style="display:none">PV Information, management</span>
@@ -165,8 +147,8 @@
                             </div>
                         </div>
 
-                           <!-- List of Permissions Categories -->
-                           <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <!-- List of Permissions Categories -->
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
                                     <h5>
@@ -181,12 +163,9 @@
                                 </div>
                             </div>
                         </div>
-                        @if (\App\Helpers\AccessRightsHelper::checkPermission('permission_categories.view') == 'inline')
-                        
-                        @endif  
 
                         <!-- List of Permissions -->
-                         <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
                                     <h5>
@@ -201,10 +180,8 @@
                                 </div>
                             </div>
                         </div>
-                        @if (\App\Helpers\AccessRightsHelper::checkPermission('permissions.view') == 'inline')
-                        
-                        @endif
-                     
+
+                        <!-- List of Groups -->
                         <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
@@ -220,12 +197,9 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- List of Groups -->
-                        @if (\App\Helpers\AccessRightsHelper::checkPermission('groups.view') == 'inline')
-                 
-                        @endif  
 
-                            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                                                <!-- List of Users -->
+                        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
                                     <h5>
@@ -241,13 +215,7 @@
                             </div>
                         </div>
 
-                        <!-- List of Users -->
-                        @if (\App\Helpers\AccessRightsHelper::checkPermission('users.view') == 'inline')
-                    
-                        @endif
-
                         <!-- List of Activity Logs -->
-                        @if (\App\Helpers\AccessRightsHelper::checkPermission('activity_logs.view') == 'inline')
                         <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
@@ -263,19 +231,8 @@
                                 </div>
                             </div>
                         </div>
-                        @endif
-                        <style>
-                            .icon {
-                                color: #0077B6;
-                                transition: color 0.3s ease;
-                            }
-                            .settings_button:hover .icon {
-                                color: #023E8A;
-                            }
-                        </style>
 
-                        <!-- Add a new box for API Documentation below the Groups box -->
-                        @if (\App\Helpers\AccessRightsHelper::checkPermission('api_documentation.view') == 'inline')
+                        <!-- List of API Documentation -->
                         <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
                             <div class="box box-default" style="border: 2px solid #bbb; margin: 10px; border-radius: 15px;">
                                 <div class="box-body text-center mt-4">
@@ -291,10 +248,25 @@
                                 </div>
                             </div>
                         </div>
-                        @endif
                     </div> <!-- end flex-wrap -->
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
+<style>
+    .icon {
+        color: #0077B6;
+        transition: color 0.3s ease;
+    }
+    .settings_button:hover .icon {
+        color: #023E8A;
+    }
+    .box {
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    .box:hover {
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    }
+</style>
